21b54e13d1fd2204cb1e714157c5d4472061bfaf
PanTwice <18742452152@163.com>
2018-07-30 10:03:34 +0800

    添加hello code相关注释

--- src/com/demo/hello/SayHello.kt
+++ src/com/demo/hello/SayHello.kt
@@ -1,11 +1,18 @@
+// 包名 和java是一样的
 package com.demo.hello
 
+// 定义类及函数
 class first(val name: String){
+
+    // 定义class里面的函数
     fun say(){
         println("hello, $name")
     }
+
 }
 
+// 主函数
 fun main(args: Array<String>){
+    // 类函数调用
     first("liu").say()
 }
