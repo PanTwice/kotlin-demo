6f4f3be178a7ee15482f1e6758cb078cc2462f62
PanTwice <18742452152@163.com>
2018-08-09 18:37:26 +0800

    添加随机数相关函数

--- src/com/demo/hello/RandomNum.kt
+++ src/com/demo/hello/RandomNum.kt
@@ -6,23 +6,24 @@ import java.util.*
  * 貌似是用的都是java的随机数函数,kotlin自己有没有随记数生成的函数呢?
  */
 fun main(args: Array<String>) {
-
+    random1()
+    random2()
 }
 
 /**
  * 使用random类
  */
 fun random1() {
-    var randomObj = Random()
-    val randomNum = randomObj.nextInt(10)
+    val randomObj = Random()
+    val randomNum = randomObj.nextInt(100)
     println(randomNum)
 }
 
 /**
  * 使用math类
  */
-fun random2(): Unit {
-    val randomNum = Math.random()*10
+fun random2() {
+    val randomNum = Math.random() * 10
     println(randomNum)
 }
 
