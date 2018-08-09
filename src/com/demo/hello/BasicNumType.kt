bbec39c0561b0e8a1a83a44f92f4ffee1a55cfce
pantwice <pantwice@163.com>
2018-08-09 22:43:20 +0800

    基本数值类型定义,类型判断,when,if和for流程控制基本用法

--- src/com/demo/hello/BasicNumType.kt
+++ src/com/demo/hello/BasicNumType.kt
@@ -12,6 +12,23 @@ package com.demo.hello
  */
 
 fun main(args: Array<String>) {
+
+    // 数值变量/常量定义
+    val longNum = 1_000L
+    val doubleNum = 1_000.0
+    val floatNum = 1_000.0f
+    // 注意0x/0b直接跟下划线的写法是错误的
+    val hexNum = 0xff_ff_00_11
+    val binaryNum = 0b10_101
+    val longNum2 = 1000L
+
+    println("1000==1_000 相等不?  "+(longNum == longNum2))
+
+
+}
+
+// 打印数值类型最大值和最小值,获取取值范围    估计特别大的是也是bigInteger类型
+fun printMaxAndMin() {
     println("输出不同类型的最大值和最小值")
     println("double max ${Double.MAX_VALUE}")
     println("double min ${Double.MIN_VALUE}")
@@ -36,23 +53,4 @@ fun main(args: Array<String>) {
     println("Byte max ${Byte.MAX_VALUE}")
     println("Byte min ${Byte.MIN_VALUE}")
     println()
-
-    // 变量/常量定义
-    val longNum = 1_000L
-    val doubleNum = 1_000.0
-    val floatNum = 1_000.0f
-    // 注意0x/0b直接跟下划线的写法是错误的
-    val hexNum = 0xff_ff_00_11
-    val binaryNum = 0b10_101
-
-    val longNum2 = 1000L
-
-    println("1000==1_000 相等不?  "+(longNum == longNum2))
-
-
-
-
-
-
-
 }
---
+++ src/com/demo/hello/ExceptionDeal.kt
@@ -0,0 +1,10 @@
+package com.demo.hello
+
+fun main(args: Array<String>) {
+    // 异常处理还是java的try..catch这样的形式,没有什么改进的样子
+    try {
+        var int = 10
+    }catch (e:Exception){
+        throw e
+    }
+}
