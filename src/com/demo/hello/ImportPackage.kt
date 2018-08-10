6c0fe7792989ddb7dea5f9af7ba3ebb709558030
PanTwice <18742452152@163.com>
2018-08-10 09:42:28 +0800

    添加junit依赖包hamcrest相关jar

---
+++ src/com/demo/hello/ImportPackage.kt
@@ -0,0 +1,13 @@
+package com.demo.hello
+
+import org.junit.Test
+import kotlin.test.assertTrue
+
+class ImportPackage {
+
+    @Test fun testPrintAny() {
+        print(" my name is kotlin ")
+        assertTrue(true)
+    }
+
+}
