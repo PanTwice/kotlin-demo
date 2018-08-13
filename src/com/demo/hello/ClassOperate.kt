3a5f9df11ce9da2e7dd2b509ee3fbfe509a7bbcb
pantwice <pantwice@163.com>
2018-08-13 23:07:46 +0800

    学习定义类相关

--- src/com/demo/hello/ClassOperate.kt
+++ src/com/demo/hello/ClassOperate.kt
@@ -19,7 +19,7 @@ class person_omit(var name: String, var age: Int) {
 
 }
 
-// 如歌构造函数有注解或者可见性修饰符,那么constructor关键字是必须的
+// 如果构造函数有注解或者可见性修饰符,比如public,那么constructor关键字是必须的
 class person_full constructor(var name: String, var age: Int) {
     init {
         println("init_full")
@@ -67,64 +67,76 @@ class InitDemo(name: String) {
 }
 
 // 类的继承,open关键字说明这个类允许继承,默认是final
-open class Base(intNum:Int){
-    open fun say(){
+open class Base(intNum: Int) {
+
+    open val size: Int =
+            intNum.also { println("Initializing size in Base: $it") }
+
+    open fun say() {
         println("it's base")
     }
 }
-// 继承车里的base(intNum是必须的)
-class ExtendBase(intNum: Int) : Base(intNum){
+
+// 继承base(intNum是必须的)
+class ExtendBase(intNum: Int) : Base(intNum) {
+
+    // 覆写变量
+    override val size: Int =
+            (super.size + intNum).also { println("Initializing size in Derived: $it") }
+
     // 覆写函数的前提是父类的函数必须是open的,和继承一个道理 ,继承的实现必须写override
     final override fun say() {
         println("it is extendBase")
     }
-}
 
-open class Bases(val name: String) {
-
-    init { println("Initializing Base") }
-
-    open val size: Int =
-            name.length.also { println("Initializing size in Base: $it") }
-}
-
-class Derived(
-        name: String,
-        val lastName: String
-) : Bases(name.capitalize().also { println("Argument for Base: $it") }) {
-
-    init { println("Initializing Derived") }
-
-    override val size: Int =
-            (super.size + lastName.length).also { println("Initializing size in Derived: $it") }
 }
 
 open class Foo {
-    open fun f() { println("Foo.f()") }
+    open fun f() {
+        println("Foo.f()")
+    }
+
     open val x: Int get() = 1
 }
 
 class Bar : Foo() {
-    override fun f() { /* …… */ }
+    override fun f() {
+        super.f()
+        println("bar.f()")
+    }
+
     override val x: Int get() = 0
 
     inner class Baz {
+        // 内部类调用外部类及父类方法
         fun g() {
-            super@Bar.f() // 调用 Foo 实现的 f()
-            println(super@Bar.x) // 使用 Foo 实现的 x 的 getter
+            // 调用 Foo 实现的 f()
+            super@Bar.f()
+            // 使用 Foo 实现的 x 的 getter
+            println(super@Bar.x)
         }
     }
 }
 
 // 很好玩的一个例子,貌似是使用了java8接口的默认函数吧 default
 open class A {
-    open fun f() { print("A") }
-    fun a() { print("a") }
+    open fun f() {
+        print("A")
+    }
+
+    fun a() {
+        print("a")
+    }
 }
 
 interface B {
-    fun f() { print("B") } // 接口成员默认就是“open”的
-    fun b() { print("b") }
+    fun f() {
+        print("B")
+    } // 接口成员默认就是“open”的
+
+    fun b() {
+        print("b")
+    }
 }
 
 class C() : A(), B {
@@ -147,6 +159,6 @@ fun main(args: Array<String>) {
     MoreConstructor("liu", 23, "man")
 
     // 注意观察基础类的初始化顺序
-    val d = Derived("hello", "world")
+    val d = ExtendBase(10)
 
 }
